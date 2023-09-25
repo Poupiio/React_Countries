102eedb839f561f0235dc283c09f878279dc0cd6
Poupiio <elise.maaiza33@gmail.com>
2023-09-25 15:32:07 +0200

    ajout articles

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
+import axios from 'axios';
+import Article from '../components/Article';
 
 const Blog = () => {
 
     const [content, setContent] = useState("");
     const [error, setError] = useState(false);
+    const [blogData, setBlogData] = useState([]);
+
+    const getData = () => {
+        axios
+            .get("http://localhost:3004/articles")
+            .then((res) => setBlogData(res.data));
+    };
+
+    useEffect(() => getData(), []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -26,11 +37,21 @@ const Blog = () => {
 
             <form onSubmit={(e) => handleSubmit(e)}>
                 <input type="text" placeholder='Nom' />
-                <textarea placeholder='Message' onChange={(e) => setContent(e.target.value)}></textarea>
+                <textarea 
+                    style={{ border: error ? "1px solid red" : "1px solid #61dafb"}}
+                    placeholder='Message'
+                    onChange={(e) => setContent(e.target.value)}>
+
+                </textarea>
                 {/* Si error est à true, on renvoie le message d'erreur */}
                 {error && <p>Veuillez écrire un minimum de 140 caractères.</p>}
                 <input type="submit" value="Envoyer" />
             </form>
+            <ul>
+                {blogData.map((article) => (
+                    <Article key={article.id} article={article} />
+                ))}
+            </ul>
         </div>
     );
 };
